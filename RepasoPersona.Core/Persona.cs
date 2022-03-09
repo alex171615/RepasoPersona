780e5ac5241843d4a1b7ba3a416be884444b97c4
alex171615 <christhianmaytaet12d1@gmail.com>
2022-03-09 15:41:13 -0300

    cracion repaso

---
+++ RepasoPersona.Core/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace RepasoPersona.Core
+{
+    public class Persona
+    {
+        public string Nombre {get;private set;}
+        public string Apellido{get;private set;}
+        public double Efectivo{get;private set;}
+
+        public Persona(string nombre, string apellido, double efectivo)
+        {
+            Nombre=nombre;
+            Apellido=apellido;
+            Efectivo=efectivo;
+        }
+        public Persona ()=> Efectivo=0;
+        public void Debito(double monto)
+        {
+            if (monto<=0)
+            {
+                throw new Exception("El monto debe ser > 0");
+                
+            }
+
+            if(Efectivo<monto)
+            {
+                throw new Exception ("Saldo insuficiente");
+                Efectivo -=monto;
+            }
+        }
+
+        public void acreditar (double monto)
+        {
+            if (monto<0)
+            {
+                throw new Exception("El monto debe ser > 0");
+            }
+             if(Efectivo>monto)
+            {
+                throw new Exception ("Saldo insuficiente");
+                Efectivo +=monto;
+            }
+        }
+    }
+}
